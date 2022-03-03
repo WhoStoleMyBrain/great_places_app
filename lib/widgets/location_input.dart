a6254144327bf38deeda9de5c1eacaf3b22bcf67
marco <marcowetter@gmx.de>
2022-03-03 17:54:15 +0100

    Added camera interaction, google maps location, google maps location to address transformation by api. Not functioning due to missing google api key.

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../helpers/location_helper.dart';
+import '../screens/maps_screen.dart';
+
+class LocationInput extends StatefulWidget {
+  final Function onSelectPlace;
+
+  LocationInput(this.onSelectPlace);
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+
+  void _showPreview(double lat, double lng) {
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: lat, longitude: lng);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _getCurrentUserLocation() async {
+    try {
+      final locData = await Location().getLocation();
+      _showPreview(locData.latitude, locData.longitude);
+      widget.onSelectPlace(locData.latitude, locData.longitude);
+    } catch (error) {
+      return;
+    }
+  }
+
+  Future<void> _selectOnMap() async {
+    final LatLng selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    _showPreview(selectedLocation.latitude, selectedLocation.longitude);
+    widget.onSelectPlace(selectedLocation.latitude, selectedLocation.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          child: _previewImageUrl == null
+              ? Text(
+                  'No location chosen or found',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  errorBuilder: (ctx, obj, stack) => Center(
+                      child: Text(
+                    'Error loading google maps preview',
+                    style: TextStyle(color: Colors.red),
+                  )),
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentUserLocation,
+              icon: Icon(
+                Icons.location_on,
+              ),
+              label: Text('Current Location'),
+              style: TextButton.styleFrom(
+                  primary: Theme.of(context).colorScheme.primary),
+            ),
+            TextButton.icon(
+              onPressed: _selectOnMap,
+              icon: Icon(
+                Icons.map,
+              ),
+              label: Text('Select on map'),
+              style: TextButton.styleFrom(
+                  primary: Theme.of(context).colorScheme.primary),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
